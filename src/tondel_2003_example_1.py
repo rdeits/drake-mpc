c469e7c7b0defb590a27df900e416a1993a32f59
Robin Deits <robin.deits@gmail.com>
2017-02-02 19:11:32 -0500

    plot critical region using iris's cdd bindings

--- src/tondel_2003_example_1.py
+++ src/tondel_2003_example_1.py
@@ -2,50 +2,12 @@ from __future__ import absolute_import, division, print_function
 
 from itertools import combinations
 import numpy as np
+import matplotlib.pyplot as plt
 import pydrake.solvers.mathematicalprogram as mp
+import irispy
 
 
-# Taking a model directly from Tondel et al. 2003, Example 1
-prog = mp.MathematicalProgram()
-
-z = prog.NewContinuousVariables(2, "z")
-x = prog.NewContinuousVariables(2, "x")
-prog.AddLinearConstraint(x[0] == 0)
-prog.AddLinearConstraint(x[1] == 0)
-
-dt = 0.05
-A = np.array([[1, dt],
-              [0, 1]])
-b = np.array([dt**2, dt])
-H = np.array([[1.079, 0.076],
-              [0.076, 1.073]])
-F = np.array([[1.109, 1.036],
-              [1.573, 1.517]])
-G = np.array([[1, 0],
-              [0, 1],
-              [-1, 0],
-              [0, -1],
-              [dt, 0],
-              [dt, dt],
-              [-dt, 0],
-              [-dt, -dt]])
-W = np.array([[1.0], [1], [1], [1], [0.5], [0.5], [0.5], [0.5]])
-S = np.array([[1.0, 1.4],
-              [0.9, 1.3],
-              [-1.0, -1.4],
-              [-0.9, -1.3],
-              [0.1, -0.9],
-              [0.1, -0.9],
-              [-0.1, 0.9],
-              [-0.1, 0.9]])
-
-for i in range(G.shape[0]):
-    prog.AddLinearConstraint(G[i,:].dot(z) <= W[i,0] + S[i,:].dot(x))
-
-prog.AddQuadraticCost(H, np.zeros(z.size), z)
-
-
-def get_objective_matrix(prog, vars, params):
+def objective_matrix(prog, vars, params):
     H = np.zeros((prog.num_vars(), prog.num_vars()))
     for binding in prog.quadratic_costs():
         print("vars", binding.variables())
@@ -64,7 +26,7 @@ def get_objective_matrix(prog, vars, params):
     H = H + H.T - np.diag(np.diag(H))
     return H
 
-def get_linconstr_matrices(prog, vars, params):
+def linconstr_matrices(prog, vars, params):
     A_elements = []
     lb_elements = []
     ub_elements = []
@@ -106,10 +68,10 @@ def linearly_independent_subset(G, active_indices):
 
 
 def critical_region(model, vars, params):
-    H = get_objective_matrix(prog, vars, params)
+    H = objective_matrix(prog, vars, params)
     z = prog.GetSolution(vars)
     x = prog.GetSolution(params)
-    G, W, S = get_linconstr_matrices(prog, vars, params)
+    G, W, S = linconstr_matrices(prog, vars, params)
 
     constraint_values = G.dot(z) - W - S.dot(x)
     active_constraint_indices = [i for (i, v) in enumerate(constraint_values) \
@@ -123,20 +85,72 @@ def critical_region(model, vars, params):
     print("GA:", GA)
 
     crmodel = mp.MathematicalProgram()
-    x = crmodel.NewContinuousVariables(len(params), "x")
+    crvars = crmodel.NewContinuousVariables(len(params), "crvars")
 
     if Ai:
-        lambdaA = -np.linalg.inv(GA.dot(np.linalg.inv(H)).dot(GA.T)).dot(WA + SA.dot(x))
+        lambdaA = -np.linalg.inv(GA.dot(np.linalg.inv(H)).dot(GA.T)).dot(WA + SA.dot(crvars))
         z = -np.linalg.inv(H).dot(GA.T).dot(lambdaA)
-    constr_expr = G.dot(z) - S.dot(x) - W
+    constr_expr = G.dot(z) - S.dot(crvars) - W
     for expr in constr_expr:
         print("constraint:", expr <= 0)
-        prog.AddLinearConstraint(expr <= 0)
-    return crmodel
+        crmodel.AddLinearConstraint(expr <= 0)
+    return crmodel, crvars
+
+def feasible_set_polyhedron(model, vars):
+    G, W, S = linconstr_matrices(model, vars, [])
+    assert S.size == 0
+    p = irispy.Polyhedron(G, W)
+    return p
 
 
-result = prog.Solve()
-assert result == mp.SolutionResult.kSolutionFound
 
-crmodel = critical_region(prog, z, x)
+if __name__ == '__main__':
+    # Taking a model directly from Tondel et al. 2003, Example 1
+    prog = mp.MathematicalProgram()
+
+    z = prog.NewContinuousVariables(2, "z")
+    x = prog.NewContinuousVariables(2, "x")
+    prog.AddLinearConstraint(x[0] == 0)
+    prog.AddLinearConstraint(x[1] == 0)
+
+    dt = 0.05
+    A = np.array([[1, dt],
+                  [0, 1]])
+    b = np.array([dt**2, dt])
+    H = np.array([[1.079, 0.076],
+                  [0.076, 1.073]])
+    F = np.array([[1.109, 1.036],
+                  [1.573, 1.517]])
+    G = np.array([[1, 0],
+                  [0, 1],
+                  [-1, 0],
+                  [0, -1],
+                  [dt, 0],
+                  [dt, dt],
+                  [-dt, 0],
+                  [-dt, -dt]])
+    W = np.array([[1.0], [1], [1], [1], [0.5], [0.5], [0.5], [0.5]])
+    S = np.array([[1.0, 1.4],
+                  [0.9, 1.3],
+                  [-1.0, -1.4],
+                  [-0.9, -1.3],
+                  [0.1, -0.9],
+                  [0.1, -0.9],
+                  [-0.1, 0.9],
+                  [-0.1, 0.9]])
+
+    for i in range(G.shape[0]):
+        prog.AddLinearConstraint(G[i,:].dot(z) <= W[i,0] + S[i,:].dot(x))
+
+    prog.AddQuadraticCost(H, np.zeros(z.size), z)
+
+    result = prog.Solve()
+    assert result == mp.SolutionResult.kSolutionFound
+
+    crmodel, crvars = critical_region(prog, z, x)
+    p = feasible_set_polyhedron(crmodel, crvars)
+    pts = np.vstack(p.generatorPoints())
+
+    plt.plot(pts[[0,1,2,3,0],0], pts[[0,1,2,3,0],1], "r.-")
+    plt.show()
 
