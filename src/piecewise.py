016f41fb1326b0ea68ca9b87d95f814fc6e69412
Robin Deits <robin.deits@gmail.com>
2017-02-21 13:05:51 -0500

    python piecewise polynomial types

---
+++ src/piecewise.py
@@ -0,0 +1,74 @@
+from __future__ import absolute_import, division, print_function
+
+import unittest
+import numpy as np
+
+
+class Piecewise(object):
+    def __init__(self, breaks, functions):
+        self.breaks = np.asarray(breaks)
+        assert all(breaks[i] <= breaks[i + 1] for i in range(len(breaks) - 1))
+        assert len(breaks) == len(functions) + 1
+        self.functions = functions
+
+    def from_above(self, x):
+        i = np.searchsorted(self.breaks, x, side="right")
+        if i <= 0 or i >= len(self.breaks):
+            raise ValueError("Input value {:g} is out of the allowable range [{:g}, {:g})".format(x, self.breaks[0], self.breaks[-1]))
+        return self.functions[i - 1](x - self.breaks[i - 1])
+
+    def from_below(self, x):
+        i = np.searchsorted(self.breaks, x, side="left")
+        if i <= 0 or i >= len(self.breaks):
+            raise ValueError(
+                "Input value {:g} is out of the allowable range ({:g}, {:g}]".format(x, self.breaks[0], self.breaks[-1]))
+        return self.functions[i - 1](x - self.breaks[i - 1])
+
+    def __call__(self, x):
+        return self.from_above(x)
+
+    def map(self, function):
+        return Piecewise(self.breaks, map(function, self.functions))
+
+
+class TestPiecewise(unittest.TestCase):
+    def test_from_above(self):
+        p = Piecewise([1, 2, 3],
+                      [lambda x: 1,
+                       lambda x: 2])
+        self.assertEqual(p.from_above(1), 1)
+        self.assertEqual(p.from_above(1.5), 1)
+        self.assertEqual(p.from_above(2), 2)
+        self.assertEqual(p.from_above(2.5), 2)
+        self.assertEqual(p(1), 1)
+        self.assertEqual(p(1.5), 1)
+        self.assertEqual(p(2), 2)
+        self.assertEqual(p(2.5), 2)
+
+    def test_from_below(self):
+        p = Piecewise([1, 2, 3],
+                      [lambda x: 1,
+                       lambda x: 2])
+        self.assertEqual(p.from_below(1.5), 1)
+        self.assertEqual(p.from_below(2), 1)
+        self.assertEqual(p.from_below(2.5), 2)
+        self.assertEqual(p.from_below(3), 2)
+
+    def test_map(self):
+        p = Piecewise([1, 2, 3],
+                      [lambda x: 1,
+                       lambda x: 2])
+        self.assertEqual(p(1), 1)
+        self.assertEqual(p(1.5), 1)
+        self.assertEqual(p(2), 2)
+        self.assertEqual(p(2.5), 2)
+
+        p2 = p.map(lambda f: lambda x: 2 * f(x))
+        self.assertEqual(p2(1), 2)
+        self.assertEqual(p2(1.5), 2)
+        self.assertEqual(p2(2), 4)
+        self.assertEqual(p2(2.5), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/polynomial.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function
+import unittest
+
+
+class Polynomial(object):
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
+
+    def __getitem__(self, idx):
+        return self.coeffs[idx]
+
+    def __call__(self, x):
+        return self.evaluate(x)
+
+    @property
+    def degree(self):
+        return len(self.coeffs) - 1
+
+    def evaluate(self, x):
+        y = self.coeffs[-1]
+        for i in range(self.degree - 1, -1, -1):
+            y = self[i] + x * y
+        return y
+
+    def derivative(self):
+        return Polynomial([i * self[i] for i in range(1, self.degree + 1)])
+
+
+class TestPolynomial(unittest.TestCase):
+    def test_polynomial_eval(self):
+        p = Polynomial([1, 2, 3, 4])
+        for x in range(-5, 5):
+            self.assertAlmostEqual(p(x), 1 + 2 * x + 3 * x**2 + 4 * x**3)
+
+    def test_polynomial_degree(self):
+        p = Polynomial([1, 2, 3, 4])
+        self.assertEqual(p.degree, 3)
+
+    def test_polynomial_derivative(self):
+        p = Polynomial([3, 1, 7, 8, 2])
+        dp = p.derivative()
+        self.assertEqual(dp.coeffs, [1, 2 * 7, 3 * 8, 4 * 2])
+
+
+if __name__ == '__main__':
+    unittest.main()
