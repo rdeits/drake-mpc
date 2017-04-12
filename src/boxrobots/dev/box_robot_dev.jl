1e68242250b48fac5ad9195b80972bb61222ed38
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-04-12 16:05:24 -0400

    more work on visualizing box robots

--- src/boxrobots/dev/box_robot_dev.jl
+++ src/boxrobots/dev/box_robot_dev.jl
@@ -1,11 +1,17 @@
 path_to_my_module = string(Base.source_dir(), "/../src/")
-println(typeof(path_to_my_module))
-println(LOAD_PATH)
-println(path_to_my_module)
 push!(LOAD_PATH, path_to_my_module)
-import BoxRobots
-using DrakeVisualizer
 
+using BoxRobots
+using DrakeVisualizer
+import ColorTypes: RGBA
 
 # attempt to visualize a state
 DrakeVisualizer.any_open_windows() || DrakeVisualizer.new_window();
+vis = Visualizer()
+
+# test constructing and drawing a polyhedron
+poly = polyhedron_from_bounds([-1,1], [-1,1])
+poly_3d = convert_polyhedron_to_3d(poly)
+green = RGBA(0.,1.0,0.0)
+
+setgeometry!(vis[:poly], GeometryData(poly_3d, green))
---
+++ src/boxrobots/src/types.jl
@@ -0,0 +1,51 @@
+immutable Surface{D}
+    position::SimpleHRepresentation{D, Float64}
+    force::SimpleHRepresentation{D, Float64}
+end
+
+# describes a single limb
+immutable LimbConfig{D}
+    name::String
+    velocity_limit::Float64
+    bounds::SimpleHRepresentation{D, Float64}
+    surface::Surface{D} # the contact surface corresponding to this limb
+end
+
+immutable BoxRobot{D}
+    mass::Float64
+    dim::Int16
+    limbs::Dict{Symbol, LimbConfig{D}}
+end
+
+immutable Environment{D}
+    surfaces::Vector{Surface{D}}
+end
+
+# Now we write down things needed for simulation, namely robot state
+type LimbState{T}
+  pos::Vector{Float64}
+  vel::Vector{Float64}
+  in_contact::Bool
+end
+
+type CentroidalDynamicsState{T}
+  pos::Vector{Float64}
+  vel::Vector{Float64}
+  # could add angular momentum later if we want
+end
+
+type BoxRobotState{T}
+  centroidal_dynamics_state::CentroidalDynamicsState{T}
+  limb_state::Dict{Symbol, LimbState{T}}
+end
+
+## INPUTS:
+type LimbInput{T}
+  acceleration::Vector{Float64}
+  force::Vector{Float64}
+  has_force::Bool # should have either acceleration or force, not both
+end
+
+type BoxRobotInput{T}
+  limb_input::Dict{Symbol, LimbInput{T}}
+end
